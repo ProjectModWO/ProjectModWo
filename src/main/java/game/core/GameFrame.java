0ce5dd6ee153efb33326312ae9bdf5927f7c3a35
kindlich <timo-klenk@web.de>
2017-10-21 22:11:06 +0200

    Init gradle

---
+++ src/main/java/game/core/GameFrame.java
@@ -0,0 +1,20 @@
+package main.game.core;/* 
+ * Copyright (C) 2017 Vindalia - All Rights Reserved 
+ * 
+ * Unauthorized copying of this file, via any medium is strictly prohibited 
+ * Proprietary and confidential 
+ * 
+ * Written by Vindalia <development@vindalia.net>, 2017 
+ */
+
+import javax.swing.*;
+import java.awt.*;
+
+public class GameFrame extends JFrame {
+
+    public GameFrame() throws HeadlessException {
+
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        setSize(300,500);
+    }
+}
