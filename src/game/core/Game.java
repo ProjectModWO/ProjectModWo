8ae7b52e333473e963470a31fa718ec3c29622a9
DartHulos <david.storzer@hotmail.de>
2017-10-21 22:02:58 +0200

    Reorganisation

--- src/game/core/Game.java
+++ src/game/core/Game.java
@@ -1,15 +1,9 @@
 package game.core;
 
-import javax.swing.JFrame;
 
-public class Game extends JFrame {
+public class Game {
 	
-	public Game() {
-		
-		setBounds(500, 200, 500, 500);
-		setVisible(true);
-	}
-	
+
 	public static void main(String[] args) {
 		
 		new Game();	
