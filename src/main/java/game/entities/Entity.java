9ce98901e5fe9b7b258854b594a9a6260b907844
kindlich <timo-klenk@web.de>
2017-10-24 22:02:39 +0200

    Entity Changes

--- src/main/java/game/entities/Entity.java
+++ src/main/java/game/entities/Entity.java
@@ -1,9 +1,41 @@
 package game.entities;
 
+import game.entities.capabilities.IMovable;
 import game.physics.Position;
+import game.utils.Vector;
 
-public abstract class Entity {
+public abstract class Entity implements IMovable{
 
     public abstract Position getPosition();
+    public double weight;
+    public Vector speed;
+    
+	@Override
+	public double getWeight() {
+		return weight;
+	}
+
+	@Override
+	public Vector getSpeed() {
+		return speed;
+	}
+
+	@Override
+	public Vector getAcceleration() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void move(Vector impulse) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void move(Vector force, double timeInMillis) {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
